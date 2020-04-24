311a7f448a046bbc360d8e2414dc7d35fec1a054
Simon Shillaker <mail@simonshillaker.com>
2020-04-24 15:23:25 +0100

    Moving in more code

--- src/sgd/README.md
+++
@@ -1,3 +0,0 @@
-# Stochastic Gradient Descent
-
-TODO
--- tasks/__init__.py
+++ tasks/__init__.py
@@ -7,15 +7,31 @@ from . import bench_tpt
 from . import data
 from . import experiments
 from . import genomics
+from . import matrix_data
 
 # Include all Faasm tasks
 from faasmcli.tasks import ns
 
 # Tasks from this repo
-ns.add_collection(bench_capacity)
-ns.add_collection(bench_mem)
-ns.add_collection(bench_time)
-ns.add_collection(bench_tpt)
 ns.add_collection(data)
 ns.add_collection(experiments)
 ns.add_collection(genomics)
+ns.add_collection(matrix_data)
+
+# Group benchmarking tasks
+bench_ns = Collection("bench")
+bench_ns.add_task(bench_capacity.max_threads)
+bench_ns.add_task(bench_time.bench_time, name="time")
+bench_ns.add_task(bench_mem.bench_mem, name="mem")
+bench_ns.add_task(bench_tpt.bench_tpt, name="tpt")
+bench_ns.add_task(bench_mem.pid_mem, name="pid")
+bench_ns.add_task(bench_mem.container_count)
+bench_ns.add_task(bench_mem.faasm_count)
+bench_ns.add_task(bench_mem.spawn_containers)
+bench_ns.add_task(bench_mem.kill_containers)
+bench_ns.add_task(bench_mem.kill_faasm)
+bench_ns.add_task(bench_mem.plot_pid_mem)
+bench_ns.add_task(bench_mem.plot_proc_mem)
+bench_ns.add_task(bench_mem.print_docker_mem)
+ns.add_collection(bench_ns)
+
