65d6e9dca50e4067a3d057c2559cfdd55a5d5fef
Simon Shillaker <scs17@ic.ac.uk>
2020-09-11 12:12:20 +0000

    Static libs

--- tasks/mxnet.py
+++ tasks/mxnet.py
@@ -36,31 +36,37 @@ def lib(ctx, clean=False):
         "-DUSE_MKL_IF_AVAILABLE=OFF",
         "-DUSE_F16C=OFF",
         "-DUSE_SSE=OFF",
+        "-DUSE_S3=OFF",
         "-DUSE_OPENMP=OFF",
         "-DUSE_OPENCV=OFF",
         "-DUSE_INTGEMM=OFF",
+        "-DUSE_TENSORRT=OFF",
         "-DUSE_OPERATOR_TUNING=OFF",
         "-DBUILD_CPP_EXAMPLES=OFF",
         "-DUSE_SIGNAL_HANDLER=OFF",
         "-DUSE_CCACHE=OFF",
         "-DUSE_CPP_PACKAGE=ON",
         "-DMXNET_BUILD_SHARED_LIBS=OFF",
+        "-DBUILD_SHARED_LIBS=OFF",
         MXNET_DIR,
     ]
 
     cmake_str = " ".join(cmake_cmd)
     print(cmake_str)
 
+    print("RUNNING CMAKE")
     res = run(cmake_str, shell=True, cwd=work_dir, env=env_vars)
     if res.returncode != 0:
         raise RuntimeError("MXNet CMake config failed ({})".format(
             res.returncode
             ))
 
-    res = run("ninja mxnet", shell=True, cwd=work_dir, env=env_vars)
+    print("RUNNING NINJA")
+    res = run("ninja -v mxnet", shell=True, cwd=work_dir, env=env_vars)
     if res.returncode != 0:
         raise RuntimeError("MXNet build failed ({})".format(res.returncode))
 
+    print("RUNNING INSTALL")
     res = run("ninja install", shell=True, cwd=work_dir)
     if res.returncode != 0:
         raise RuntimeError("MXNet install failed ({})".format(res.returncode))
