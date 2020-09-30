17580f4e8f0385bcf5e06b2cf1664db8f3d0ae51
Simon Shillaker <scs17@ic.ac.uk>
2020-09-30 15:47:52 +0000

    Added actions and formatting fixes

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,4 +1,3 @@
-
 def Settings(**kwargs):
     faasm_interpreter = "/usr/local/code/faasm/venv/bin/python"
     return {"interpreter_path": faasm_interpreter}
--- ansible/roles/billing/files/metrics.py
+++ ansible/roles/billing/files/metrics.py
@@ -13,7 +13,6 @@ SLEEP_INTERVAL_SECS = 2
 
 
 class SystemStatsMonitor(object):
-
     def initialise(self):
         # cpu_percent will return zeroes to start with, so we want to make an initial call
         # https://psutil.readthedocs.io/en/latest/#psutil.cpu_percent
@@ -22,10 +21,7 @@ class SystemStatsMonitor(object):
 
         # Log one-off measurements
         cpu_freq = psutil.cpu_freq()
-        stats = [
-            ("CPU_COUNT", psutil.cpu_count()),
-            ("CPU_FREQ", cpu_freq.max)
-        ]
+        stats = [("CPU_COUNT", psutil.cpu_count()), ("CPU_FREQ", cpu_freq.max)]
 
         self._log_stats(stats)
 
@@ -36,20 +32,24 @@ class SystemStatsMonitor(object):
         vmem = psutil.virtual_memory()
         swap = psutil.swap_memory()
 
-        stats.extend([
-            ("MEMORY_USED", vmem.used / ONE_MB),
-            ("MEMORY_ACTIVE", vmem.active / ONE_MB),
-            ("MEMORY_USED_PCT", vmem.percent),
-            ("MEMORY_AVAILABLE", vmem.available / ONE_MB),
-            ("SWAP_USED", swap.used / ONE_MB),
-        ])
+        stats.extend(
+            [
+                ("MEMORY_USED", vmem.used / ONE_MB),
+                ("MEMORY_ACTIVE", vmem.active / ONE_MB),
+                ("MEMORY_USED_PCT", vmem.percent),
+                ("MEMORY_AVAILABLE", vmem.available / ONE_MB),
+                ("SWAP_USED", swap.used / ONE_MB),
+            ]
+        )
 
         # DISK
         disk = psutil.disk_io_counters(perdisk=False)
-        stats.extend([
-            ("DISK_READ_MB", disk.read_bytes / ONE_MB),
-            ("DISK_WRITE_MB", disk.write_bytes / ONE_MB),
-        ])
+        stats.extend(
+            [
+                ("DISK_READ_MB", disk.read_bytes / ONE_MB),
+                ("DISK_WRITE_MB", disk.write_bytes / ONE_MB),
+            ]
+        )
 
         # CPU percent
         cpu_pct = psutil.cpu_percent(percpu=False)
@@ -61,44 +61,54 @@ class SystemStatsMonitor(object):
         # CPU times
         cpu_times = psutil.cpu_times(percpu=False)
         cpu_times_per_cpu = psutil.cpu_times(percpu=True)
-        stats.extend([
-            ("CPU_TIME_IDLE", cpu_times.idle),
-            ("CPU_TIME_IOWAIT", cpu_times.iowait),
-            ("CPU_TIME_USER", cpu_times.user),
-            ("CPU_TIME_SYSTEM", cpu_times.system),
-        ])
+        stats.extend(
+            [
+                ("CPU_TIME_IDLE", cpu_times.idle),
+                ("CPU_TIME_IOWAIT", cpu_times.iowait),
+                ("CPU_TIME_USER", cpu_times.user),
+                ("CPU_TIME_SYSTEM", cpu_times.system),
+            ]
+        )
         for cpu_idx, t in enumerate(cpu_times_per_cpu):
-            stats.extend([
-                ("CPU_{}_TIME_IDLE".format(cpu_idx), t.idle),
-                ("CPU_{}_TIME_IOWAIT".format(cpu_idx), t.iowait),
-                ("CPU_{}_TIME_USER".format(cpu_idx), t.user),
-                ("CPU_{}_TIME_SYSTEM".format(cpu_idx), t.system),
-            ])
+            stats.extend(
+                [
+                    ("CPU_{}_TIME_IDLE".format(cpu_idx), t.idle),
+                    ("CPU_{}_TIME_IOWAIT".format(cpu_idx), t.iowait),
+                    ("CPU_{}_TIME_USER".format(cpu_idx), t.user),
+                    ("CPU_{}_TIME_SYSTEM".format(cpu_idx), t.system),
+                ]
+            )
 
         # CPU percent
         cpu_times_pct = psutil.cpu_times_percent(percpu=False)
         cpu_times_pct_per_cpu = psutil.cpu_times_percent(percpu=True)
-        stats.extend([
-            ("CPU_PCT_IDLE", cpu_times_pct.idle),
-            ("CPU_PCT_IOWAIT", cpu_times_pct.iowait),
-            ("CPU_PCT_USER", cpu_times_pct.user),
-            ("CPU_PCT_SYSTEM", cpu_times_pct.system),
-        ])
+        stats.extend(
+            [
+                ("CPU_PCT_IDLE", cpu_times_pct.idle),
+                ("CPU_PCT_IOWAIT", cpu_times_pct.iowait),
+                ("CPU_PCT_USER", cpu_times_pct.user),
+                ("CPU_PCT_SYSTEM", cpu_times_pct.system),
+            ]
+        )
         for cpu_idx, p in enumerate(cpu_times_pct_per_cpu):
-            stats.extend([
-                ("CPU_{}_PCT_IDLE".format(cpu_idx), p.idle),
-                ("CPU_{}_PCT_IOWAIT".format(cpu_idx), p.iowait),
-                ("CPU_{}_PCT_USER".format(cpu_idx), p.user),
-                ("CPU_{}_PCT_SYSTEM".format(cpu_idx), p.system),
-            ])
+            stats.extend(
+                [
+                    ("CPU_{}_PCT_IDLE".format(cpu_idx), p.idle),
+                    ("CPU_{}_PCT_IOWAIT".format(cpu_idx), p.iowait),
+                    ("CPU_{}_PCT_USER".format(cpu_idx), p.user),
+                    ("CPU_{}_PCT_SYSTEM".format(cpu_idx), p.system),
+                ]
+            )
 
         # NETWORK
         network = psutil.net_io_counters(pernic=False)
 
-        stats.extend([
-            ("NET_SENT_MB", network.bytes_sent / ONE_MB),
-            ("NET_RECV_MB", network.bytes_recv / ONE_MB),
-        ])
+        stats.extend(
+            [
+                ("NET_SENT_MB", network.bytes_sent / ONE_MB),
+                ("NET_RECV_MB", network.bytes_recv / ONE_MB),
+            ]
+        )
 
         self._log_stats(stats)
 
@@ -107,7 +117,9 @@ class SystemStatsMonitor(object):
         dt_now = time.time()
         with open(OUTPUT_FILE, "a") as fh:
             for stat_name, stat_value in stats:
-                fh.write("{:.2f} {} {}\n".format(dt_now, stat_name, stat_value))
+                fh.write(
+                    "{:.2f} {} {}\n".format(dt_now, stat_name, stat_value)
+                )
 
 
 if __name__ == "__main__":
