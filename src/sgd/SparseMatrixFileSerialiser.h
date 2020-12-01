3bed62fe7bc1b4acf79766e537efd71234212ddf
Simon Shillaker <scs17@ic.ac.uk>
2020-12-01 12:26:05 +0000

    Add sparse matrix files for safe keeping

---
+++ src/sgd/SparseMatrixFileSerialiser.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <faasm/matrix.h>
+#include <faasm/sgd.h>
+#include <shared_mutex>
+
+using namespace faasm;
+
+namespace storage {
+class SparseMatrixFileSerialiser : public SparseMatrixSerialiser
+{
+  public:
+    explicit SparseMatrixFileSerialiser(const SparseMatrix<double>& matIn);
+
+    void writeToFile(const std::string& directory);
+
+    static SparseMatrix<double> readFromFiles(const std::string& directory);
+};
+}
